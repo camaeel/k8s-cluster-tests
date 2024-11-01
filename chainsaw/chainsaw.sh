b87d6efe9872bd9534b626cada27cc8cf90c4a57
camaeel <12999736+camaeel@users.noreply.github.com>
2024-11-02 00:51:13 +0100

    test: cert-manager
    Signed-off-by: camaeel <12999736+camaeel@users.noreply.github.com>

--- chainsaw/chainsaw.sh
+++ chainsaw/chainsaw.sh
@@ -5,5 +5,6 @@ if [[ -f ./.env ]]; then
 fi
 
 kubectl delete ns -l chainsaw-test
+kubectl delete clusterissuer -l chainsaw-test
 
-chainsaw test  --pause-on-failure --config=./config.yaml --values=./values.yaml $@
+chainsaw test  --pause-on-failure --config=./config.yaml --values values.yaml $@
