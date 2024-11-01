f213c8846ddd955e2f4e25339b488cb84cb4823b
camaeel <12999736+camaeel@users.noreply.github.com>
2024-11-02 00:07:40 +0100

    test: setup config
    Signed-off-by: camaeel <12999736+camaeel@users.noreply.github.com>

--- chainsaw/chainsaw.sh
+++ chainsaw/chainsaw.sh
@@ -6,4 +6,4 @@ fi
 
 kubectl delete ns -l chainsaw-test
 
-chainsaw test  --pause-on-failure --config=./config.yaml $@
+chainsaw test  --pause-on-failure --config=./config.yaml --values=./values.yaml $@
